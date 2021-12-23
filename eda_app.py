e39826f55abce4827214d3faf380f431f341d180
minsu0717 <alstnalstnalstnrlaalstn@gmail.com>
2021-12-23 17:56:37 +0900

    최종

--- eda_app.py
+++ eda_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from PIL import Image
 
 def run_eda_app():
     df = pd.read_csv('data/NBA_season1718_salary.csv',index_col=0)
@@ -15,5 +18,39 @@ def run_eda_app():
     else :
         st.dataframe(df)
         
-    st.subheader('연봉 범위')
-    st.slider('보고싶은 범위를 선택해주세요',min_value=df['season17_18'].min(),max_value=df['season17_18'].max())
+    st.subheader('선수 검색 하기')
+    player = st.text_input('이름을 적어주세요')
+    st.write(df.loc[df['Player'].str.lower().str.contains(player.lower()),])
+    
+    st.subheader('연봉 상위 10명과 하위 10명')
+    menu_2 = ['상위 10명','하위 10명']
+    choice =st.radio('선택',menu_2)
+    if choice == '상위 10명':
+        st.write(df.sort_values('season17_18',ascending=False).head(10))
+    else :
+        st.write(df.sort_values('season17_18',ascending=False).tail(10))
+        
+    st.subheader('어떤 팀이 선수와 계약을 많이 했는지 보기')
+    fig1=plt.figure(figsize=(15,10))
+    sns.countplot(data=df,x='Tm',order=df['Tm'].value_counts().index)
+    st.pyplot(fig1)
+    st.write(df.groupby('Tm').count().sort_values('Player',ascending=False)['Player'])
+    
+    st.subheader('어떤 팀이 선수를 비싸게 영입했는지 보기')
+    team = df.groupby('Tm')['season17_18'].mean()
+    team=team.sort_values(ascending=False)
+    fig1=plt.figure(figsize=(15,10))
+    plt.bar(team.index,team)
+    st.pyplot(fig1)
+    st.write(team)
+    
+    st.subheader('팀 순위 보기')
+    menu_3 = ['동부','서부']
+    choice_region=st.radio('선택',menu_3)
+    if choice_region == '동부':
+        st.image('data/nba_2.png')
+    else:
+        st.image('data/nba_1.png')
+        
+    st.subheader('분석 결과 선수와 많이 계약을 한 것 보다 돈을 많이 쓰는 팀이\
+        좋은 성적을 낸다는 것을 알 수 있다.')
